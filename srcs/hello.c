be747287397722c37deaa66447e18a355fd55138
Lachlan Read <lread@pop-os.localdomain>
2021-10-11 16:01:12 +1030

    Revert "Added 2nd line"
    This reverts commit 812cdb8b928bca314dc4501644a9b6406cf65123.
    
    Reverting files..

--- srcs/hello.c
+++ srcs/hello.c
@@ -2,8 +2,12 @@
 
 int main(void)
 {
+<<<<<<< HEAD
 	ft_putstr_fd("H3llo W0rld!\n");
 	ft_putstr_fd("I am a V1rUs\n");
 	ft_putstr_fd("Jack is here now\n");
+=======
+	ft_putstr_fd("H3llo W0rld!");
+>>>>>>> parent of 812cdb8 (Added 2nd line)
 	return (0);
 }
